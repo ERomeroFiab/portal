2e8ae2c4f00525dc93e807b98a89363fd89c3280
eromerowant <eromerowant@gmail.com>
2022-02-17 13:35:44 -0300

    plantilla y autenticación funcionando

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,164 +21,210 @@
 <body class="sidebar-mini">
     <div class="wrapper">
 
-        <div class="sidebar" data-color="blue">
-            <!-- Tip 1: You can change the color of the sidebar using: data-color="blue | green | orange | red | yellow" -->
-            <div class="logo">
-                <a href="#" class="simple-text logo-mini" title="Sistema integrado de gestión">
-                    <img src="assets/img/logo-md.png">
-                </a>
-                <!--Nombre de la empresa-->
-                <a href="#" class="simple-text logo-normal" title="Sistema integrado de gestión">Fiabilis</a>
-                <div class="navbar-minimize">
-                    <button id="minimizeSidebar" class="btn btn-simple btn-icon btn-neutral btn-round">
-                        <i class="now-ui-icons text_align-center visible-on-sidebar-regular"></i>
-                        <i class="now-ui-icons design_bullet-list-67 visible-on-sidebar-mini"></i>
-                    </button>
+        @auth
+
+            <div class="sidebar" data-color="blue">
+                <!-- Tip 1: You can change the color of the sidebar using: data-color="blue | green | orange | red | yellow" -->
+                <div class="logo">
+                    <a href="#" class="simple-text logo-mini" title="Sistema integrado de gestión">
+                        <img src="assets/img/logo-md.png">
+                    </a>
+                    <!--Nombre de la empresa-->
+                    <a href="#" class="simple-text logo-normal" title="Sistema integrado de gestión">Fiabilis</a>
+                    <div class="navbar-minimize">
+                        <button id="minimizeSidebar" class="btn btn-simple btn-icon btn-neutral btn-round">
+                            <i class="now-ui-icons text_align-center visible-on-sidebar-regular"></i>
+                            <i class="now-ui-icons design_bullet-list-67 visible-on-sidebar-mini"></i>
+                        </button>
+                    </div>
+                </div>
+                <div class="sidebar-wrapper ps-container ps-theme-default">
+                    <ul class="nav">
+                        <!-- <li class="">
+                            <a href="#">
+                                <span class="sidebar-mini-icon"><i class="now-ui-icons business_briefcase-24"></i></span>
+                                <p class="sidebar-normal">Antecedentes</p>
+                            </a>
+                        </li> -->
+                        <li class="">
+                            <a href="#">
+                                <span class="sidebar-mini-icon"><i class="now-ui-icons business_badge"></i></span>
+                                <p class="sidebar-normal">Pestaña</p>
+                            </a>
+                        </li>
+                        <li class="active">
+                            <a data-toggle="collapse" href="#sidebar-admin" aria-expanded="false" class="collapsed">
+                                <span class="sidebar-mini-icon"><i class="now-ui-icons ui-2_settings-90"></i></span>
+                                <p class="sidebar-normal">Administración <b class="caret"></b></p>
+                            </a>
+                            <div class="clearfix"></div>
+                            <div class="collapse" id="sidebar-admin">
+                                <ul class="nav">
+                                    <li class="">
+                                        <a href="#">
+                                            <span class="sidebar-mini-icon"><i class="fas fa-plus"></i></span>
+                                            <p class="sidebar-normal">Crear usuario</p>
+                                        </a>
+                                    </li>
+                                    <li class="active">
+                                        <a href="#">
+                                            <span class="sidebar-mini-icon">U</span>
+                                            <p class="sidebar-normal">Usuarios</p>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                    <div class="ps-scrollbar-x-rail" style="left: 0px; bottom: 0px;">
+                        <div class="ps-scrollbar-x" tabindex="0" style="left: 0px; width: 0px;"></div>
+                    </div>
+                    <div class="ps-scrollbar-y-rail" style="top: 0px; right: 0px;">
+                        <div class="ps-scrollbar-y" tabindex="0" style="top: 0px; height: 0px;"></div>
+                    </div>
                 </div>
             </div>
-            <div class="sidebar-wrapper ps-container ps-theme-default">
-                <ul class="nav">
-                    <!-- <li class="">
-                        <a href="#">
-                            <span class="sidebar-mini-icon"><i class="now-ui-icons business_briefcase-24"></i></span>
-                            <p class="sidebar-normal">Antecedentes</p>
-                        </a>
-                    </li> -->
-                    <li class="">
-                        <a href="#">
-                            <span class="sidebar-mini-icon"><i class="now-ui-icons business_badge"></i></span>
-                            <p class="sidebar-normal">Pestaña</p>
-                        </a>
-                    </li>
-                    <li class="active">
-                        <a data-toggle="collapse" href="#sidebar-admin" aria-expanded="false" class="collapsed">
-                            <span class="sidebar-mini-icon"><i class="now-ui-icons ui-2_settings-90"></i></span>
-                            <p class="sidebar-normal">Administración <b class="caret"></b></p>
-                        </a>
-                        <div class="clearfix"></div>
-                        <div class="collapse" id="sidebar-admin">
-                            <ul class="nav">
-                                <li class="">
-                                    <a href="#">
-                                        <span class="sidebar-mini-icon"><i class="fas fa-plus"></i></span>
-                                        <p class="sidebar-normal">Crear usuario</p>
+
+            <div class="main-panel">
+                <!-- Navbar -->
+                <nav class="navbar navbar-expand-lg navbar-transparent  navbar-absolute bg-primary fixed-top">
+                    <div class="container-fluid">
+                        <div class="navbar-wrapper">
+                            <div class="navbar-toggle">
+                                <button type="button" class="navbar-toggler">
+                                    <span class="navbar-toggler-bar bar1"></span>
+                                    <span class="navbar-toggler-bar bar2"></span>
+                                    <span class="navbar-toggler-bar bar3"></span>
+                                </button>
+                            </div>
+                            <a class="navbar-brand">Portal Cliente</a>
+                        </div>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation"
+                            aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-bar navbar-kebab"></span>
+                            <span class="navbar-toggler-bar navbar-kebab"></span>
+                            <span class="navbar-toggler-bar navbar-kebab"></span>
+                        </button>
+                        <div class="collapse navbar-collapse justify-content-end" id="navigation">
+
+                            <ul class="navbar-nav">
+
+
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" id="navUserDropdown"
+                                        data-toggle="dropdown">
+                                        <!-- <img height="16" src="#"> -->
+                                        <span class="mx-1">{{ auth()->user()->name }} </span>
                                     </a>
-                                </li>
-                                <li class="active">
-                                    <a href="#">
-                                        <span class="sidebar-mini-icon">U</span>
-                                        <p class="sidebar-normal">Usuarios</p>
-                                    </a>
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navUserDropdown">
+                                        <a class="dropdown-item" href="" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                            <i class="fas fa-sign-out-alt"></i> Cerrar Sesión
+                                        </a>
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                            @csrf
+                                        </form>
+                                    </div>
                                 </li>
                             </ul>
                         </div>
-                    </li>
-                </ul>
-                <div class="ps-scrollbar-x-rail" style="left: 0px; bottom: 0px;">
-                    <div class="ps-scrollbar-x" tabindex="0" style="left: 0px; width: 0px;"></div>
-                </div>
-                <div class="ps-scrollbar-y-rail" style="top: 0px; right: 0px;">
-                    <div class="ps-scrollbar-y" tabindex="0" style="top: 0px; height: 0px;"></div>
-                </div>
-            </div>
-        </div>
+                    </div>
+                </nav>
+                <!-- End Navbar -->
 
-        <div class="main-panel">
-            <!-- Navbar -->
-            <nav class="navbar navbar-expand-lg navbar-transparent  navbar-absolute bg-primary fixed-top">
-                <div class="container-fluid">
-                    <div class="navbar-wrapper">
-                        <div class="navbar-toggle">
-                            <button type="button" class="navbar-toggler">
-                                <span class="navbar-toggler-bar bar1"></span>
-                                <span class="navbar-toggler-bar bar2"></span>
-                                <span class="navbar-toggler-bar bar3"></span>
-                            </button>
+                <div class="panel-header panel-header-sm"></div>
+
+                <!-- Start Content -->
+                <div class="content">
+                    @yield('content')
+                </div> <!-- End Content -->
+
+                <footer class="footer">
+                    <div class="container-fluid">
+                        <div class="copyright">
+                            © 2022, Developed by <a href="https://www.fiabilis.cl/" target="_blank">Fiabilis</a>.
                         </div>
-                        <a class="navbar-brand">Portal Cliente</a>
                     </div>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation"
-                        aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-bar navbar-kebab"></span>
-                        <span class="navbar-toggler-bar navbar-kebab"></span>
-                        <span class="navbar-toggler-bar navbar-kebab"></span>
-                    </button>
-                    <div class="collapse navbar-collapse justify-content-end" id="navigation">
+                </footer>
+            </div>
 
-                        <ul class="navbar-nav">
+        @else
 
+            <div class="sidebar" data-color="blue">
 
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navUserDropdown"
-                                    data-toggle="dropdown">
-                                    <!-- <img height="16" src="#"> -->
-                                    <span class="mx-1">Peter</span>
-                                </a>
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navUserDropdown">
-                                    <a class="dropdown-item" href="">
-                                        <i class="fas fa-sign-out-alt"></i> Cerrar Sesión
-                                    </a>
-                                </div>
-                            </li>
-                        </ul>
+                <div class="logo">
+                    <a href="#" class="simple-text logo-mini" title="Sistema integrado de gestión">
+                        <img src="assets/img/logo-md.png">
+                    </a>
+                    <!--Nombre de la empresa-->
+                    <a href="#" class="simple-text logo-normal" title="Sistema integrado de gestión">Fiabilis</a>
+                    <div class="navbar-minimize">
+                        <button id="minimizeSidebar" class="btn btn-simple btn-icon btn-neutral btn-round">
+                            <i class="now-ui-icons text_align-center visible-on-sidebar-regular"></i>
+                            <i class="now-ui-icons design_bullet-list-67 visible-on-sidebar-mini"></i>
+                        </button>
                     </div>
                 </div>
-            </nav>
-            <!-- End Navbar -->
 
-            <div class="panel-header panel-header-sm"></div>
+            </div>
 
-            <!-- Start Content -->
-            <div class="content">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="card">
-                            <div class="card-header">
-                                <h2>Bienvenido</h2>
+            <div class="main-panel">
+                <!-- Navbar -->
+                <nav class="navbar navbar-expand-lg navbar-transparent  navbar-absolute bg-primary fixed-top">
+                    <div class="container-fluid">
+                        <div class="navbar-wrapper">
+                            <div class="navbar-toggle">
+                                <button type="button" class="navbar-toggler">
+                                    <span class="navbar-toggler-bar bar1"></span>
+                                    <span class="navbar-toggler-bar bar2"></span>
+                                    <span class="navbar-toggler-bar bar3"></span>
+                                </button>
                             </div>
-                            <div class="card-body">
-                                <div class="row">
-                                    <div class="col-12">
-                                        <table class="table-striped nowrap dataTable no-footer" style="min-width:100%">
-                                            <thead>
-                                                <th>Nombre</th>
-                                                <th>E-mail</th>
-                                                <th>Rol</th>
-                                            </thead>
-                                            <tbody>
-                                                <tr>
-                                                    <td>Administrador Principal</td>
-                                                    <td>admin@admin.cl</td>
-                                                    <td>Administrador</td>
-                                                </tr>
-                                                <tr>
-                                                    <td>Pedro</td>
-                                                    <td>pedro@admin.cl</td>
-                                                    <td>Consultor</td>
-                                                </tr>
-                                                <tr>
-                                                    <td>Juan</td>
-                                                    <td>juan@admin.cl</td>
-                                                    <td>Cliente</td>
-                                                </tr>
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                </div>
-                            </div>
-                        </div> <!-- End card -->
-                    </div>
-                </div>
-            </div> <!-- End Content -->
+                            <a class="navbar-brand">Portal Cliente</a>
+                        </div>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation"
+                            aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-bar navbar-kebab"></span>
+                            <span class="navbar-toggler-bar navbar-kebab"></span>
+                            <span class="navbar-toggler-bar navbar-kebab"></span>
+                        </button>
+                        <div class="collapse navbar-collapse justify-content-end" id="navigation">
 
-            <footer class="footer">
-                <div class="container-fluid">
-                    <div class="copyright">
-                        © 2022, Developed by <a href="https://www.fiabilis.cl/" target="_blank">Fiabilis</a>.
+                            <ul class="navbar-nav">
+
+                                <li class="nav-item">
+                                    <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+                                </li>
+                                <li class="nav-item">
+                                    @if (Route::has('register'))
+                                        <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                                    @endif
+                                </li>
+
+                            </ul>
+                        </div>
                     </div>
-                </div>
-            </footer>
-        </div>
+                </nav>
+                <!-- End Navbar -->
+
+                <div class="panel-header panel-header-sm"></div>
+
+                <!-- Start Content -->
+                <div class="content">
+                    @yield('content')
+                </div> <!-- End Content -->
+
+                <footer class="footer">
+                    <div class="container-fluid">
+                        <div class="copyright">
+                            © 2022, Developed by <a href="https://www.fiabilis.cl/" target="_blank">Fiabilis</a>.
+                        </div>
+                    </div>
+                </footer>
+            </div>
+            
+        @endauth
+
+
     </div>
     <!--   Core JS Files   -->
     <script src="{{asset('assets/js/core/jquery.min.js?v=1645015120')}}"></script>
