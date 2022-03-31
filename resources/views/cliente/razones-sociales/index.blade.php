d90921b5a9a2257c5edfdd9e77a33b5b7bcb37ce
Eduardo <eromero@fiabilis.cl>
2022-03-31 14:35:31 -0300

    menos datos en menu razones sociales index

--- resources/views/cliente/razones-sociales/index.blade.php
+++ resources/views/cliente/razones-sociales/index.blade.php
@@ -14,20 +14,20 @@
                 </div>
                 <div class="row">
                     @foreach ($razones_sociales as $razon_social)
-                        <div class="col-3 my-3">
+                        <div class="col-4 my-3 text-center">
 
-                            <a class="inside_card" href="{{route('cliente.razones-sociales.show', ['id' => $razon_social->id])}}">
+                            <a class="inside_card rounded" href="{{route('cliente.razones-sociales.show', ['id' => $razon_social->id])}}">
                                 <div class="card-body">
                                     <div class="row">
                                         <div class="col-12">
                                             <h6 class="mb-5"><b>{{ $razon_social->nombre }}</b></h6>
                                             <p>Rut: <b>{{ $razon_social->rut ?? "-" }}</b></p>
-                                            <p>Ciudad: <b>{{ $razon_social->ciudad ?? "-" }}</b></p>
+                                            {{-- <p>Ciudad: <b>{{ $razon_social->ciudad ?? "-" }}</b></p>
                                             <p>Código Postal: <b>{{ $razon_social->codigo_postal ?? "-" }}</b></p>
                                             <p>Dirección: <b>{{ $razon_social->direccion ?? "-" }}</b></p>
                                             <p>N° de Cuenta: <b>{{ $razon_social->numero_de_cuenta_bancaria ?? "-" }}</b></p>
                                             <p>Banco: <b>{{ $razon_social->banco ?? "-" }}</b></p>
-                                            <p>Cuenta: <b>{{ $razon_social->tipo_de_cuenta ?? "-" }}</b></p>
+                                            <p>Cuenta: <b>{{ $razon_social->tipo_de_cuenta ?? "-" }}</b></p> --}}
                                         </div>
                                     </div>
                                 </div>
