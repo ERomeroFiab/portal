bb5fd42bf0b5f5a46646e18168326845b8e97e0a
Rodrigo Díaz <rdaz@fiabilischile.local>
2022-04-25 09:33:08 -0400

    login portal

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,5 @@
-@extends('layouts.app')
 
-@section('content')
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -71,4 +70,3 @@
         </div>
     </div>
 </div>
-@endsection
